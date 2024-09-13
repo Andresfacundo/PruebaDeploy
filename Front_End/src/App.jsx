eb8bfb7387c6416b2c7ad719562d4aeb8b1c2bb6
Andres Facundo <andresfelipefacundo@gmail.com>
2024-09-13 08:16:09 -0500

    its moved folders services and pages

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom' //router
-import {Welcome} from './Components/Layouts/Pages/Welcome/Welcome'
+import {Welcome} from './Components/Pages/Welcome/Welcome'
 import { Login } from './Components/Pages/Login/Login'
 import { Guest } from './Components/Pages/Guest/Guest'
 import { SignUp } from './Components/Pages/SignUp/SignUp'
