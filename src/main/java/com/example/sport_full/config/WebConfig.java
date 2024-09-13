94e899f5245f0ed6c6c8b816ac6f71949fe51e84
Andres Facundo <andresfelipefacundo@gmail.com>
2024-09-13 07:25:42 -0500

    modifieed path on webConfig

--- src/main/java/com/example/sport_full/config/WebConfig.java
+++ src/main/java/com/example/sport_full/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://prueba-deploy-theta-ten.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
--- src/main/java/com/example/sport_full/controllers/UserControllers.java
+++ src/main/java/com/example/sport_full/controllers/UserControllers.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://prueba-deploy-theta-ten.vercel.app/")
 @RestController
 @RequestMapping("/auth")
 public class UserControllers {
