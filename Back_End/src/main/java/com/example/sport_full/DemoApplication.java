244ae71f58762b50092c159f44bd559f37f184ae
andres <andresfelipefacundo@gmail.com>
2024-10-24 10:53:17 -0500

    se refactoriza el nombre del archivo principal

--- Back_End/src/main/java/com/example/sport_full/DemoApplication.java
+++ Back_End/src/main/java/com/example/sport_full/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplicationTests {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplicationTests.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
