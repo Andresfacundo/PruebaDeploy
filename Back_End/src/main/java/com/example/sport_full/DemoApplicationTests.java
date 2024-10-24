5c4b04430374caf8cf661515a1be17e1dfad3768
andres <andresfelipefacundo@gmail.com>
2024-10-24 10:47:27 -0500

    se cambia el nombre del archivo que ejecuta el proyecto

--- Back_End/src/main/java/com/example/sport_full/DemoApplicationTests.java
+++ Back_End/src/main/java/com/example/sport_full/DemoApplicationTests.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplicationTests {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplicationTests.class, args);
 	}
 
 }
