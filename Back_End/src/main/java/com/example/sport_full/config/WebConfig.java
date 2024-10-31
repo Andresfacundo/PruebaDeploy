7d752f3a69d9841f46e3b88d778ac31b58e1f328
andres <andresfelipefacundo@gmail.com>
2024-10-31 16:11:06 -0500

    sv 7

--- Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
+++ Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://prueba-deploy-ten.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
 
     }
 }
