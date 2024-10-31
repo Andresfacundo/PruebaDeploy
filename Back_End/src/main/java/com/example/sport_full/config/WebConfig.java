456049775e305630031c8b65225b4611359936ac
andres <andresfelipefacundo@gmail.com>
2024-10-31 15:05:52 -0500

    sv4

--- Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
+++ Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://prueba-deploy-ten.vercel.app/")
+                .allowedOrigins("https://prueba-deploy-ten.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
--- Back_End/src/main/java/com/example/sport_full/services/EmailServices.java
+++ Back_End/src/main/java/com/example/sport_full/services/EmailServices.java
@@ -22,7 +22,7 @@ public class EmailServices {
         message.setTo(email);
         message.setSubject("Verificación de Correo");
         message.setText("Para verificar tu correo, por favor haz clic en el siguiente enlace: "
-                + "https://prueba-deploy-theta-ten.vercel.app//auth/verify?token=" + verificationToken);
+                + "https://prueba-deploy-theta-ten.vercel.app/auth/verify?token=" + verificationToken);
         mailSender.send(message);
     }
 }
