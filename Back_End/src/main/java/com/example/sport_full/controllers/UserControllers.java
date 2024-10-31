4f577b81a4ea6ffd6b94b38e7b3703f67c520e39
andres <andresfelipefacundo@gmail.com>
2024-10-31 15:22:21 -0500

    sv 5

--- Back_End/src/main/java/com/example/sport_full/controllers/UserControllers.java
+++ Back_End/src/main/java/com/example/sport_full/controllers/UserControllers.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://prueba-deploy-ten.vercel.app")
 @RestController
 @RequestMapping("/auth")
 public class UserControllers {
