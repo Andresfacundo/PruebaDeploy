af26414b66e26051b486c9b12ca506312b38e59f
andres <andresfelipefacundo@gmail.com>
2024-10-31 14:00:02 -0500

    Docker 1

--- src/main/java/com/example/sport_full/repositories/IGestorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sport_full.repositories;
-
-import com.example.sport_full.models.GestorModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IGestorRepository extends JpaRepository<GestorModels, Long> {
-}
